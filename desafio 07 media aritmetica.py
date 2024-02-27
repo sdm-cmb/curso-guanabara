3825dfc66cc833d484a35b2af597b96f57ff6884
sdm-cmb <59738106+sdm-cmb@users.noreply.github.com>
2024-02-27 16:03:33 -0300

    Create desafio 07 media aritmetica.py

---
+++ desafio 07 media aritmetica.py
@@ -0,0 +1,4 @@
+N1 = float(input('digite sua primeira nota:'))
+N2 = float(input('Digite sua segunda nota:'))
+M = (N1+N2)/2
+print(' soma das suas notas {:1.2f} e {:1.2f} da a media de {:1.2f}'.format(N1, N2, M))
