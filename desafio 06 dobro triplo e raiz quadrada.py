d5aabb943e623e1b579cce684a447eb8a62092dd
sdm-cmb <59738106+sdm-cmb@users.noreply.github.com>
2024-02-27 16:03:20 -0300

    Create desafio 06 dobro triplo e raiz quadrada.py

---
+++ desafio 06 dobro triplo e raiz quadrada.py
@@ -0,0 +1,6 @@
+N = int(input('digite um numero: '))
+D = N*2
+T = N*3
+R = N**(1/2)
+print('o Dobro de {} é {}'.format(N, D))
+print('o triplo de {} é {} \nA raiz quadrada de {} é igual a {:1.2f}'. format(N, T, N, R))#
