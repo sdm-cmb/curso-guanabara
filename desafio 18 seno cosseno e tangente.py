dabacdb146217c7b50ed60ef966c99103c57f9f5
sdm-cmb <59738106+sdm-cmb@users.noreply.github.com>
2024-03-06 18:38:47 -0300

    desafio 18 seno cosseno e tangente

---
+++ desafio 18 seno cosseno e tangente.py
@@ -0,0 +1,6 @@
+import math
+ang = float(input('digite un ângulo:'))
+cos = math.cos(math.radians(ang))
+sen = math.sin(math.radians(ang))
+tan = math.tan(math.radians(ang))
+print('a medida do cosseno é {:.3f} Do seno é {:.3f} Da tangente é {:.3f}'.format(cos, sen, tan)) 
